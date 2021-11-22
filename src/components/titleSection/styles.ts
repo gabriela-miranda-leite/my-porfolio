e4cbdff52adc35c2513201097204c2e6de0260a1
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-11-22 19:45:42 -0300

    styles title

--- src/components/titleSection/styles.ts
+++ src/components/titleSection/styles.ts
@@ -3,20 +3,31 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 3rem;
   height: 3rem;
+  position: absolute;
+  display: flex;
+`;
+
+export const SquareGrey = styled.div`
+  height: 1.5rem;
+  width: 1.5rem;
+  background-color: ${({theme}) => theme.colors.text};
 `;
 
 export const SquareBlue = styled.div`
-  position: relative;
+  position: absolute;
+  right: 0.75rem;
+  top: 0.75rem;
   height: 1.5rem;
   width: 1.5rem;
   background-color: ${({theme}) => theme.colors.second};
 `;
 
-export const SquareGrey = styled.div`
+export const Title = styled.h3`
   position: absolute;
-  right: 50%;
-  top: 50%;
-  height: 1.5rem;
-  width: 1.5rem;
-  background-color: ${({theme}) => theme.colors.text};
+  font-weight: 500;
+  font-family: 'Ubuntu';
+  font-size: 2.2rem;
+  color: ${({theme}) => theme.colors.text};
+  left: 3.5rem;
+  top: 0rem;
 `;
