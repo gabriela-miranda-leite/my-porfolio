ed51961e72af2219c84d8dfd3265882f0ca1253a
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-12-09 00:25:05 -0300

    adding container position relative

--- src/components/animationRocket/index.tsx
+++ src/components/animationRocket/index.tsx
@@ -2,9 +2,9 @@ import * as S from './styles';
 
 export const AnimationRocket = () => {
   return (
-    <>
+    <S.Container>
       <S.Space src="assets/space.png" />
       <S.Rocket src="assets/rocket.png" />
-    </>
+    </S.Container>
   );
 };
