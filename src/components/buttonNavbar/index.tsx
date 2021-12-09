71ed718673bc7b500ab6c46f5979ad612429a5dc
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-12-08 21:10:00 -0300

    button navbar mobile

---
+++ src/components/buttonNavbar/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+export const ButtonNavbar = () => {
+  return (
+    <S.Button>
+      <S.Bar />
+      <S.Bar />
+      <S.Bar />
+    </S.Button>
+  );
+};
---
+++ src/components/buttonNavbar/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  border-radius: 1rem;
+  height: 5rem;
+  width: 5rem;
+  background-color: transparent;
+  border: 3px solid ${({theme}) => theme.colors.second};
+  padding: 1.2rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Bar = styled.div`
+  width: 3.5rem;
+  height: 0.2rem;
+  background-color: ${({theme}) => theme.colors.text};
+`;
