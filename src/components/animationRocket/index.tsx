3742b36e98c47fd397152ae2c9a970393fb32280
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-12-19 22:08:53 -0300

    adding alt images

--- src/components/animationRocket/index.tsx
+++ src/components/animationRocket/index.tsx
@@ -3,8 +3,8 @@ import * as S from './styles';
 export const AnimationRocket = () => {
   return (
     <S.Container>
-      <S.Space src="assets/space.png" />
-      <S.Rocket src="assets/rocket.png" />
+      <S.Space src="assets/space.png" alt="espaço sideral" />
+      <S.Rocket src="assets/rocket.png" alt="foguete" />
     </S.Container>
   );
 };
