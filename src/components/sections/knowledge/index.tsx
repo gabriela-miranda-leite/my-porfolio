94da052b73d4cf1cd6455c2c93ff9937df85f24c
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-12-20 01:46:00 -0300

    create section knowledge

---
+++ src/components/sections/knowledge/index.tsx
@@ -0,0 +1,105 @@
+import {useState, useEffect} from 'react';
+
+import Slider from 'react-slick';
+import {TitleSection} from '../../titleSection';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import * as S from './styles';
+
+export const Knowledge = () => {
+  const [indexLogo, setIndexLogo] = useState<number>(-1);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: true,
+    focusOnSelect: true,
+    slidesToScroll: 3,
+    slidesToShow: resizeScreen(),
+  };
+
+  const dataLogos = [
+    {img: 'assets/logos/logo-css.png', alt: 'Logo CSS', text: 'css'},
+    {
+      img: 'assets/logos/logo-html.png',
+      alt: 'Logo HTML',
+      text: 'HTML é uma linguagem de marcação, é utilizada para fins estruturais. HTML encapsula ou marca dados dentro de tags HTML, o navegador então lê e interpreta o conteúdo e consegue exibir diferentemente, títulos, parágrafos, links, tabelas e etc. 8 meses de experiência',
+    },
+    {
+      img: 'assets/logos/logo-javascript.png',
+      alt: 'Logo Javascript',
+      text: 'js',
+    },
+    {img: 'assets/logos/logo-react.png', alt: 'Logo React', text: 'react'},
+    {img: 'assets/logos/logo-next.png', alt: 'Logo Next', text: 'next'},
+    {img: 'assets/logos/logo-sass.png', alt: 'Logo SASS', text: 'sass'},
+    {
+      img: 'assets/logos/logo-flutter.png',
+      alt: 'Logo Flutter',
+      text: 'flutter',
+    },
+    {
+      img: 'assets/logos/logo-typescript.png',
+      alt: 'Logo Typescript',
+      text: 'ts',
+    },
+    {
+      img: 'assets/logos/logo-styled-components.png',
+      alt: 'Logo Styled Components',
+      text: 'styled components',
+    },
+  ];
+
+  function resizeScreen() {
+    return width < 400 ? 3 : width < 530 ? 4 : 6;
+  }
+
+  return (
+    <S.Container>
+      <S.DesktopWrapper>
+        <S.LogoWrapper>
+          <TitleSection title="CONHECIMENTOS" />
+          <S.GridLogos>
+            {dataLogos.map((logo, index) => (
+              <li key={index} onClick={() => setIndexLogo(index)}>
+                <S.Logo src={logo.img} alt={logo.alt} />
+              </li>
+            ))}
+          </S.GridLogos>
+        </S.LogoWrapper>
+        <S.Content>
+          <S.Carousel>
+            <Slider {...settings}>
+              {dataLogos.map((logo, index) => (
+                <div key={index} onClick={() => setIndexLogo(index)}>
+                  <S.Logo src={logo.img} alt={logo.alt} />
+                </div>
+              ))}
+            </Slider>
+          </S.Carousel>
+
+          <S.InfoWrapper indexLogo={indexLogo}>
+            {indexLogo === -1 ? (
+              <>
+                <h4>*Selecione o card</h4>
+                <S.KnowledgeImg
+                  src="assets/knowledge.png"
+                  alt="Conhecimentos"
+                />
+              </>
+            ) : (
+              <p>{dataLogos[indexLogo].text}</p>
+            )}
+          </S.InfoWrapper>
+        </S.Content>
+      </S.DesktopWrapper>
+    </S.Container>
+  );
+};
---
+++ src/components/sections/knowledge/styles.ts
@@ -0,0 +1,149 @@
+import styled from 'styled-components';
+
+interface InfoLogo {
+  indexLogo: number;
+}
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  background: ${({theme}) => theme.colors.knowledge};
+  padding: 5rem;
+  margin: auto;
+
+  @media (min-width: 750px) {
+    padding: 8rem;
+  }
+`;
+
+export const Content = styled.div`
+  .slick-dots li.slick-active button::before {
+    opacity: 0.75;
+    color: ${({theme}) => theme.colors.primary};
+  }
+`;
+
+export const Logo = styled.img`
+  width: 6rem;
+  height: 6rem;
+
+  @media (min-width: 750px) {
+    width: 8rem;
+    height: 8rem;
+  }
+
+  @media (min-width: 1070px) {
+    width: 10rem;
+    height: 10rem;
+  }
+`;
+
+export const Carousel = styled.div`
+  @media (min-width: 1200px) {
+    display: none;
+  }
+`;
+
+export const LogoWrapper = styled.div`
+  @media (min-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const GridLogos = styled.ul`
+  display: none;
+
+  @media (min-width: 1200px) {
+    display: block;
+    list-style: none;
+    column-count: 3;
+
+    li {
+      padding: 2rem;
+
+      &:hover {
+        filter: drop-shadow(0 0 1rem ${({theme}) => theme.colors.text});
+      }
+    }
+`;
+
+export const DesktopWrapper = styled.div`
+  @media (min-width: 1200px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20rem;
+  }
+`;
+
+export const InfoWrapper = styled.div<InfoLogo>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: ${({indexLogo}) => (indexLogo === -1 ? 'center' : 'start')};
+
+  background: ${({theme}) => theme.colors.second};
+  margin-top: 5rem;
+  padding: 1rem;
+  width: 100%;
+
+  h4 {
+    width: 100%;
+    display: flex;
+    font-size: 1.6rem;
+    font-weight: 500;
+  }
+
+  p {
+    font-size: 1.8rem;
+    font-weight: 400;
+  }
+
+  @media (min-width: 750px) {
+    padding: 2rem;
+
+    h4 {
+      font-size: 2.2rem;
+    }
+
+    p {
+      font-size: 2.2rem;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    align-items: ${({indexLogo}) => (indexLogo === -1 ? 'center' : 'start')};
+    justify-content: start;
+
+    padding: 5rem;
+    margin-top: 0rem;
+    width: 50rem;
+    height: 50rem;
+
+    h4 {
+      font-size: 2.2rem;
+    }
+
+    p {
+      font-size: 2.4rem;
+    }
+  }
+`;
+
+export const KnowledgeImg = styled.img`
+  width: 10rem;
+  padding-top: 2rem;
+
+  @media (min-width: 750px) {
+    width: 20rem;
+    padding-top: 3rem;
+  }
+
+  @media (min-width: 1070px) {
+    width: 25rem;
+    padding-top: 3rem;
+  }
+`;
