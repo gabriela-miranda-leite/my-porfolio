b9a86dae80927e52e3b13e596b06f1a662f7500f
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-12-20 23:20:28 -0300

    create section projects

---
+++ src/components/sections/projects/index.tsx
@@ -0,0 +1,95 @@
+import {useCallback, useEffect, useState} from 'react';
+
+import DataGithub from '../../../services/github';
+import {TitleSection} from '../../titleSection';
+
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import * as S from './styles';
+
+interface DataProject {
+  name: string;
+  html_url: string;
+}
+
+export const Projects = () => {
+  const [dataGit, setDataGit] = useState<DataProject[]>([]);
+  const [indexProject, setIndexProject] = useState<number>(-1);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: true,
+    focusOnSelect: true,
+    slidesToScroll: 1,
+    slidesToShow: 2,
+  };
+
+  const getDataGithub = useCallback(async () => {
+    const result = await DataGithub.getData();
+
+    if (!result.ok) {
+      return;
+    }
+
+    setDataGit(result.data);
+  }, []);
+
+  useEffect(() => {
+    getDataGithub();
+  }, [getDataGithub]);
+
+  return (
+    <S.DesktopWrapper>
+      <S.Container>
+        <TitleSection title="PROJETOS" />
+        <S.Desktop>
+          <S.Carousel>
+            <Slider {...settings}>
+              {dataGit.map((project, index) => (
+                <>
+                  <S.CardProject
+                    key={index}
+                    onClick={() => setIndexProject(index)}
+                  >
+                    <p>{project.name}</p>
+                  </S.CardProject>
+                </>
+              ))}
+            </Slider>
+          </S.Carousel>
+
+          <S.Content indexProject={indexProject}>
+            {indexProject === -1 ? (
+              <>
+                <h4>*Tecnologias Utilizadas</h4>
+                <S.TechImg src="assets/web.png" alt="web" />
+              </>
+            ) : (
+              <>
+                <p>{dataGit[indexProject].name}</p>
+                <a href={dataGit[indexProject].html_url}>mais informações</a>
+              </>
+            )}
+          </S.Content>
+
+          <S.GridProjects>
+            {dataGit.map((project, index) => (
+              <li key={index} onClick={() => setIndexProject(index)}>
+                <S.CardProject
+                  key={index}
+                  onClick={() => setIndexProject(index)}
+                >
+                  <p>{project.name}</p>
+                </S.CardProject>
+              </li>
+            ))}
+          </S.GridProjects>
+        </S.Desktop>
+      </S.Container>
+    </S.DesktopWrapper>
+  );
+};
---
+++ src/components/sections/projects/styles.ts
@@ -0,0 +1,193 @@
+import styled from 'styled-components';
+
+interface InfoProject {
+  indexProject: number;
+}
+
+export const Container = styled.section.attrs({
+  id: 'projects',
+})`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  padding: 5rem;
+  margin: auto;
+
+  .slick-dots li button::before {
+    color: ${({theme}) => theme.colors.text};
+  }
+
+  .slick-dots li.slick-active button::before {
+    color: ${({theme}) => theme.colors.second};
+  }
+
+  @media (min-width: 750px) {
+    padding: 8rem;
+  }
+
+  /* @media (min-width: 1300px) {
+    padding: 8rem 0;
+  } */
+`;
+
+export const Carousel = styled.div`
+  @media (min-width: 1300px) {
+    display: none;
+  }
+`;
+
+export const CardProject = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: end;
+
+  background: ${({theme}) => theme.colors.second};
+  width: 10rem;
+  height: 8rem;
+  padding: 0.5rem;
+  cursor: pointer;
+
+  p {
+    font-size: 1.4rem;
+    font-weight: 700;
+    font-family: 'Ubuntu';
+  }
+
+  @media (min-width: 420px) {
+    width: 15rem;
+    height: 10rem;
+  }
+
+  @media (min-width: 530px) {
+    width: 20rem;
+    height: 12rem;
+  }
+
+  @media (min-width: 750px) {
+    width: 20rem;
+
+    p {
+      font-size: 1.8rem;
+    }
+  }
+`;
+
+export const Content = styled.div<InfoProject>`
+  display: flex;
+  flex-direction: column;
+  align-items: ${({indexProject}) =>
+    indexProject === -1 ? 'center' : 'start'};
+
+  background: ${({theme}) => theme.colors.primary};
+  margin-top: 5rem;
+  padding: 1rem;
+  width: 100%;
+
+  h4 {
+    width: 100%;
+    display: flex;
+    font-size: 1.6rem;
+    font-weight: 500;
+  }
+
+  p {
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+
+  a {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+
+    color: ${({theme}) => theme.colors.textDark};
+
+    font-size: 1.4rem;
+    font-family: 'Ubuntu';
+    font-weight: 500;
+    transition-delay: 0.2s;
+
+    &:hover {
+      color: ${({theme}) => theme.colors.text};
+    }
+  }
+  @media (min-width: 750px) {
+    padding: 2rem;
+
+    h4 {
+      font-size: 2.2rem;
+    }
+
+    p {
+      font-size: 2rem;
+    }
+  }
+
+  @media (min-width: 1300px) {
+    padding: 5rem;
+    margin-top: 0rem;
+    width: 50rem;
+    height: 50rem;
+
+    h4 {
+      font-size: 2.2rem;
+    }
+
+    p {
+      font-size: 2.2rem;
+    }
+  }
+`;
+
+export const TechImg = styled.img`
+  width: 10rem;
+  padding-top: 2rem;
+
+  @media (min-width: 750px) {
+    width: 20rem;
+    padding-top: 3rem;
+  }
+
+  @media (min-width: 1070px) {
+    width: 25rem;
+    padding-top: 3rem;
+  }
+`;
+
+export const GridProjects = styled.ul`
+  display: none;
+
+  @media (min-width: 1300px) {
+    display: block;
+    list-style: none;
+    column-count: 3;
+    padding-left: 1rem;
+
+    li {
+      transition: 0.3s ease;
+      transition-delay: 0.2s;
+      opacity: 0.7;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+export const DesktopWrapper = styled.div`
+  @media (min-width: 1300px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20rem;
+  }
+`;
+
+export const Desktop = styled.div`
+  @media (min-width: 1300px) {
+    display: flex;
+  }
+`;
