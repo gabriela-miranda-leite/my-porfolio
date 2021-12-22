5c45edd8e1978b746fa910123e1d805983120434
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-12-22 01:48:02 -0300

    update text who i am

--- src/components/sections/whoIAm/index.tsx
+++ src/components/sections/whoIAm/index.tsx
@@ -13,10 +13,11 @@ export const WhoIAm = () => {
           <TitleSection title="QUEM SOU" />
           <S.Text>
             Opa bom dia, sou Gabriela Miranda. Estou cursando Engenharia da
-            computação. e também tenho reservado um tempo para conhecer novas
+            computação e também tenho reservado um tempo para conhecer novas
             tecnologias. <br /> <br />
             Iniciei no Front-end em 2021, e estou feliz com essa escolha vejo
-            vários caminhos.
+            vários caminhos. As tecnologias que tenho estudado é ReactJS e me
+            aprofundando na linguagem Javascript.
           </S.Text>
         </S.TextContent>
       </S.Content>
