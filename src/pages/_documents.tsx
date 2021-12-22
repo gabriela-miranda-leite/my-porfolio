3551a59e9d15090eae1efeb21a0d6e61d3e90372
gabrielamiranda11.10 <gabrielamiranda11.10@gmail.com>
2021-12-22 17:51:58 -0300

    adding render()

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -1,10 +1,14 @@
-import Document, {DocumentContext, DocumentInitialProps} from 'next/document';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -27,5 +31,16 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
