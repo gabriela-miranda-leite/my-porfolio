a516f93b05e5f314be00c3b685c18d64bf6aade1
Gabriela Miranda <gabrielamiranda11.10@gmail.com>
2021-12-22 02:50:59 -0300

    create text in knowledges

--- src/components/sections/knowledge/index.tsx
+++ src/components/sections/knowledge/index.tsx
@@ -26,34 +26,68 @@ export const Knowledge = () => {
   };
 
   const dataLogos = [
-    {img: 'assets/logos/logo-css.png', alt: 'Logo CSS', text: 'css'},
+    {
+      img: 'assets/logos/logo-css.png',
+      alt: 'Logo CSS',
+      text: 'é usado para estilizar elementos escritos em uma linguagem de marcação como HTML. O CSS separa o conteúdo da representação visual do site.',
+      title: 'CSS',
+      projects: 'let-me-ask, portfolio, landing-pages,...',
+    },
     {
       img: 'assets/logos/logo-html.png',
       alt: 'Logo HTML',
-      text: 'HTML é uma linguagem de marcação, é utilizada para fins estruturais. HTML encapsula ou marca dados dentro de tags HTML, o navegador então lê e interpreta o conteúdo e consegue exibir diferentemente, títulos, parágrafos, links, tabelas e etc. 8 meses de experiência',
+      text: ' é o componente básico da web, ele permite inserir o conteúdo e estabelecer a estrutura básica de um website.',
+      title: 'HTML',
+      projects: 'let-me-ask, portfolio, landing-pages,...',
     },
     {
       img: 'assets/logos/logo-javascript.png',
       alt: 'Logo Javascript',
-      text: 'js',
+      text: 'é uma linguagem de programação de alto nível criada, a princípio, para ser executada em navegadores e manipular comportamentos de páginas web.',
+      title: 'JavaScript',
+      projects: 'let-me-ask, portfolio, landing-pages,...',
     },
-    {img: 'assets/logos/logo-react.png', alt: 'Logo React', text: 'react'},
-    {img: 'assets/logos/logo-next.png', alt: 'Logo Next', text: 'next'},
-    {img: 'assets/logos/logo-sass.png', alt: 'Logo SASS', text: 'sass'},
+    {
+      img: 'assets/logos/logo-react.png',
+      alt: 'Logo React',
+      text: 'é uma biblioteca front-end e tem como um de seus objetivos facilitar a conexão entre diferentes partes de uma página, portanto seu funcionamento acontece através do que chamamos de componentes.',
+      title: 'React.js',
+      projects: 'let-me-ask, portfolio, landing-pages',
+    },
+    {
+      img: 'assets/logos/logo-next.png',
+      alt: 'Logo Next',
+      text: 'é um framework para React que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para aplicativos da web.',
+      title: 'Next.js',
+      projects: 'portfolio, landing-pages',
+    },
+    {
+      img: 'assets/logos/logo-sass.png',
+      alt: 'Logo SASS',
+      text: 'é uma linguagem de extensão do CSS. A sua ideia é adicionar recursos especiais como variáveis, mixins, funções e operações e outras opções variadas que iremos comentar.',
+      title: 'Sass',
+      projects: 'let-me-ask',
+    },
     {
       img: 'assets/logos/logo-flutter.png',
       alt: 'Logo Flutter',
-      text: 'flutter',
+      text: 'é uma biblioteca que utiliza a linguagem Dart para criação de aplicações móveis Cross Platform. Pode criar aplicações para Android e iOS compartilhando parte do código escrito.',
+      title: 'Flutter',
+      projects: 'Utilizei em projetos da faculdade.',
     },
     {
       img: 'assets/logos/logo-typescript.png',
       alt: 'Logo Typescript',
-      text: 'ts',
+      text: 'é um superconjunto de JavaScript, ou seja, um conjunto de ferramentas e formas mais eficientes de escrever código JavaScript, adicionando recursos que não estão presentes de maneira nativa na linguagem.',
+      title: 'Typescript',
+      projects: 'let-me-ask, portfolio',
     },
     {
       img: 'assets/logos/logo-styled-components.png',
       alt: 'Logo Styled Components',
-      text: 'styled components',
+      text: 'é uma biblioteca construída para desenvolvedores React e React Native. Ele permite que você use estilos de nível de componente em seus aplicativos.',
+      title: 'Styled-components',
+      projects: 'portfolio, landing-pages',
     },
   ];
 
@@ -92,7 +126,12 @@ export const Knowledge = () => {
                 <S.KnowledgeImg src="assets/carousel.png" alt="Conhecimentos" />
               </>
             ) : (
-              <p>{dataLogos[indexLogo].text}</p>
+              <p>
+                <strong>{dataLogos[indexLogo].title}</strong>
+                <br />
+                <br />
+                {dataLogos[indexLogo].text}
+              </p>
             )}
           </S.InfoWrapper>
         </S.Content>
